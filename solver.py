3ea7997aaa76beb527df297da7bab3eabf7d8c85
infreezy <57916366+infreezy@users.noreply.github.com>
2020-03-24 05:05:16 +0100

    Update solver.py

--- solver.py
+++ solver.py
@@ -10,15 +10,15 @@ for i in range(90):
 	p = process(f'echo {letter} | ltrace ./vodka',shell=True)
 	result = p.recvall().decode()
 	match = re.findall(r'(strcmp\([\"\\n]*, "(.+)"\))',result) 
-	try : 
-		# Regex sucks so I had to adapt ¯\_(ツ)_/¯
-		if len(match)>1:
-			case=len(match)-1
-		else:
-			case=0
-		alnum += match[case][1]
-	except Exception:
-		print('why the fuck is this happening bruv')
+	
+	# Regex sucks so I had to adapt ¯\_(ツ)_/¯
+	if len(match)>1:
+		case=len(match)-1
+	else:
+		case=0
+	
+	alnum += match[case][1]
+
 
 flag = flagP.findall(alnum)[0]
 process(f'echo {flag} >> flag.txt',shell=True)
